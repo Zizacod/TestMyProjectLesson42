d792042a4a0aedfe8bfe18f8eb28b2caf1772fcf
Aziz Seidametov <pathfinder.r51offroad@gmail.com>
2025-04-06 11:44:31 +0200

    Commit 2

---
+++ src/task_02/Class4.java
@@ -0,0 +1,7 @@
+package task_02;
+
+public class Class4 {
+    public static void main(String[] args) {
+        System.out.println("Мой четвертый класс");
+    }
+}
---
+++ src/task_02/Class6.java
@@ -0,0 +1,7 @@
+package task_02;
+
+public class Class6 {
+    public static void main(String[] args) {
+        System.out.println("Мой шестой класс");
+    }
+}
